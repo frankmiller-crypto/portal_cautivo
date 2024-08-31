20f617146f582e0848d0083f0f4b4a1449516182
frankmiller-crypto <onker.fc@gmail.com>
2024-08-31 16:42:56 -0600

    script.php

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php 
+
+$base_grant_url = 'https://n83.network-auth.com/splash/grant';
+$user_continue_url = 'http://www.google.com';
+
+$grant_url=$base_grant_url . "?continue_url=" . urlencode($user_continue_url);
+
+header("location: $grant_url");
+ ?>
