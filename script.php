7e5b4c8573b4b13fffc3b223e465788ff6cd1109
frankmiller-crypto <onker.fc@gmail.com>
2024-08-31 16:46:09 -0600

    cambio a script .php a n27

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php 
 
-$base_grant_url = 'https://n83.network-auth.com/splash/grant';
+$base_grant_url = 'https://n27.network-auth.com/splash/grant';
 $user_continue_url = 'http://www.google.com';
 
 $grant_url=$base_grant_url . "?continue_url=" . urlencode($user_continue_url);
